b07684b7e079f029bfd5077acf4d90d728817e33
matt.sh <162527598+ProProgramer123@users.noreply.github.com>
2026-09-03 15:11:55 -0400

    Make ransom loss safe: remove shutdown/BSOD behavior

--- Overlay.cs
+++ Overlay.cs
@@ -1,6 +1,4 @@
-﻿using NAudio.Wave;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
+using NAudio.Wave;
 
 namespace rans0m
 {
@@ -16,58 +14,40 @@ namespace rans0m
             public const uint SWP_NOSIZE = 0x0001;
             public const uint SWP_NOACTIVATE = 0x0010;
 
-            [DllImport("user32.dll")]
+            [System.Runtime.InteropServices.DllImport("user32.dll")]
             public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
         }
 
-        // ----------------------------- CONSTRUCTOR AND OVERLAY SETUP -----------------------------
         public Overlay() { InitializeComponent(); }
         protected override CreateParams CreateParams
         {
             get
             {
                 CreateParams cp = base.CreateParams;
-                cp.ExStyle |= 0x00000020; // WS_EX_TRANSPARENT
-                cp.ExStyle |= 0x00080000; // WS_EX_LAYERED
+                cp.ExStyle |= 0x00000020;
+                cp.ExStyle |= 0x00080000;
                 return cp;
             }
         }
 
-
-
-
-
-
-        // ----------------------------- RANSOM PHASES -----------------------------
-
-        /// <summary>
-        /// First phase of Ransom
-        /// </summary>
-        /// <returns>true if the mouse moved during the warning phase</returns>
         public async Task<bool> RansomWarning()
         {
             WaveOut spawnSound = SoundHelper.Create(Properties.Resources.spawn);
             spawnSound.Play();
 
-            // Shows Ransom's face randomly on the screen
             Global.RandomPosControl(pc_ransom);
             pc_ransom.Visible = true;
 
             await Task.Delay(500);
-            // Mouse spy phase
             Global.lastRegisteredMousePos = MousePosition;
             Global.spyingMouse = true;
 
-            // Hide Ransom's face and show the warning sign
             pc_stopsign.Visible = true;
             pc_ransom.Visible = false;
 
             await Task.Delay(500);
-            // End of spy phase
-
             Global.spyingMouse = false;
 
-            // Hide the warning sign, show Ransom face's on the center with a red background
             pc_stopsign.Visible = false;
             pc_ransom.Visible = true;
             Global.CenterControl(pc_ransom);
@@ -78,23 +58,19 @@ namespace rans0m
             this.BackColor = this.TransparencyKey;
             pc_ransom.Visible = false;
 
-            return MousePosition != Global.lastRegisteredMousePos; // Returns true if the mouse moved during the spy phase
+            return MousePosition != Global.lastRegisteredMousePos;
         }
 
-        /// <summary>
-        /// Second phase of Ransom, Jumpscare+Downloading effect
-        /// </summary>
         public async Task DownloadJumpscare()
         {
             WaveOut attackSound = SoundHelper.Create(Properties.Resources.attack);
             attackSound.Play();
 
             Global.CenterControl(pc_attack);
-            Point attack_center = pc_attack.Location; // Store the center position of the attack image for the shake effect
+            Point attack_center = pc_attack.Location;
             pc_attack.Visible = true;
             this.BackColor = Color.DarkRed;
 
-            // Ransom Face Shake Effect
             new Thread(async () =>
             {
                 for (int i = 0; i <= 25; i++)
@@ -107,14 +83,12 @@ namespace rans0m
 
             await Task.Delay(800);
 
-            // Downloading screen
             pc_ransom.Visible = false;
             pc_attack.Visible = false;
 
             WaveOut installSound = SoundHelper.Create(Properties.Resources.install);
             installSound.Play();
 
-            // Background signs effect
             new Thread(async () =>
             {
                 List<PictureBox> list = new();
@@ -138,7 +112,6 @@ namespace rans0m
                 }
                 catch { }
 
-                // Clear the background signs
                 foreach (PictureBox item in list)
                 {
                     try { this.Invoke(() => item.Dispose()); }
@@ -148,13 +121,12 @@ namespace rans0m
 
             Global.CenterControl(txt_download);
             Global.CenterControl(pb_download);
-            pb_download.Location = new Point(pb_download.Location.X, pb_download.Location.Y+50); // Add a lil offset
+            pb_download.Location = new Point(pb_download.Location.X, pb_download.Location.Y + 50);
 
             txt_download.Visible = true;
             pb_download.Visible = true;
             pb_download.Value = 100;
 
-            // Text and download bar shake/glitch effect
             new Thread(() =>
             {
                 for (int i = 0; i <= 40; i++)
@@ -182,12 +154,8 @@ namespace rans0m
             pb_download.Value = 0;
         }
 
-        /// <summary>
-        /// Third phase of Ransom, the actual ransom, plays the music, shows the Ransomed window, etc...
-        /// </summary>
         public async Task<bool> Ransomed()
         {
-            // Red Flash
             this.BackColor = Color.Red;
             new Thread(async () =>
             {
@@ -196,7 +164,7 @@ namespace rans0m
                     for (int i = 0; i <= 50; i++)
                     {
                         await Task.Delay(1);
-                        this.Invoke(() => this.Opacity = 1 - (i * 2 / 100.0) );
+                        this.Invoke(() => this.Opacity = 1 - (i * 2 / 100.0));
                     }
                     this.Invoke(() =>
                     {
@@ -207,14 +175,13 @@ namespace rans0m
                 catch { }
             }) { IsBackground = true }.Start();
 
-            // OST
             WaveOut layer1 = SoundHelper.Create(Properties.Resources.layer1);
             WaveOut layer2 = SoundHelper.Create(Properties.Resources.layer2);
             WaveOut layer3 = SoundHelper.Create(Properties.Resources.layer3);
 
             Global.ransomLeft = 500;
             Global.underRansom = true;
-            Global.RansomPayed = () => // Ransom payed event
+            Global.RansomPayed = () =>
             {
                 layer1.Stop();
                 layer2.Stop();
@@ -223,7 +190,6 @@ namespace rans0m
                 this.Invoke(ResetRansom);
             };
 
-            // Random flashing Ransom faces
             new Thread(async () =>
             {
                 while (Global.underRansom)
@@ -253,7 +219,6 @@ namespace rans0m
                 }
             }) { IsBackground = true }.Start();
 
-            // Shows the main ransom window
             Ransomed ransomedForm = new Ransomed();
             ransomedForm.Show();
 
@@ -269,61 +234,47 @@ namespace rans0m
             await Task.Delay(26000);
             if (!Global.underRansom) return false;
 
-            // If the code reaches here, this means the user didn't pay the ransom in time 
             ransomedForm.Close();
             return true;
         }
 
         /// <summary>
-        /// Shows the jumpscare and crashes/shutdowns the computer depending if it's started as admin or not
+        /// Shows the losing jumpscare and ends the ransom game without
+        /// shutting down Windows, killing processes, or creating a critical process.
         /// </summary>
-        public async Task CrashJumpscare()
+        public async Task LoseGame()
         {
             WaveOut attackSound = SoundHelper.Create(Properties.Resources.attack);
             attackSound.Play();
 
             this.BackColor = Color.DarkRed;
             Global.CenterControl(pc_attack);
-            Point attack_center = pc_attack.Location; // Store the center position of the attack image for the shake effect
+            Point attack_center = pc_attack.Location;
             pc_attack.Visible = true;
-            // Shake effect
+
             new Thread(async () =>
             {
                 for (int i = 0; i <= 25; i++)
                 {
                     await Task.Delay(10);
-                    try { this.Invoke(() => pc_attack.Location = new Point(attack_center.X + Global.rng.Next(-40, 40), attack_center.Y + Global.rng.Next(-40, 40))); }
+                    try
+                    {
+                        this.Invoke(() => pc_attack.Location = new Point(
+                            attack_center.X + Global.rng.Next(-40, 40),
+                            attack_center.Y + Global.rng.Next(-40, 40)));
+                    }
                     catch { break; }
                 }
             }) { IsBackground = true }.Start();
 
             await Task.Delay(1000);
 
-            if (Global.IsAdministrator())
-            {
-                // Basically morphs the process into a critical process, then kills it, causing a BSOD
-                Global.IntoCriticalProcess();
-                this.Close();
-            } 
-            else
-            {
-                // If not admin, just shutdown the computer
-                Process.Start("shutdown", "/s /t 0");
-            }
+            // Losing the ransom is now just a game-over state.
+            // ResetRansom removes the .gold files and clears the ransom state.
+            ResetRansom();
+            Close();
         }
 
-
-
-
-
-
-
-
-        // ----------------------------- CORE -----------------------------
-
-        /// <summary>
-        /// Reset the app to it's ready state
-        /// </summary>
         public void ResetRansom()
         {
             GoldCoinManager.DeleteAllCoins();
@@ -343,49 +294,39 @@ namespace rans0m
             this.BackColor = this.TransparencyKey;
         }
 
-        /// <summary>
-        /// Summons Ransom, triggers each step of the ransom process
-        /// </summary>
         public async void SpawnRansom()
         {
             if (!Global.canAttack) return;
             Global.canAttack = false;
 
             bool mouseMoved = await RansomWarning();
-            if (mouseMoved) // User moved the mouse
+            if (mouseMoved)
             {
                 try { GoldCoinManager.CreateRandomCoins(8); }
                 catch { }
 
                 await DownloadJumpscare();
 
-                if (await Ransomed()) // If user didn't pay the ransom in time
+                if (await Ransomed())
                 {
                     Global.underRansom = false;
-                    await CrashJumpscare();
-                    this.Close();
+                    await LoseGame();
                 }
             }
-            else ResetRansom(); // User didn't move the mouse, dodged the ransom
+            else ResetRansom();
         }
 
-        /// <summary>
-        /// Ransom loop, keeps summoning ransom randomly
-        /// </summary>
         private async void RansomLoop()
         {
             while (!this.IsDisposed)
             {
-                try { this.Invoke(SpawnRansom); } // Invoke via the UI thread to avoid cross-thread exceptions (RansomLoop will be called only from another thread soooo)
+                try { this.Invoke(SpawnRansom); }
                 catch { break; }
 
-                await Task.Delay(Global.rng.Next(Global.minRansomTime*1000, Global.maxRansomTime*1000)); // Ransom Debounce
+                await Task.Delay(Global.rng.Next(Global.minRansomTime * 1000, Global.maxRansomTime * 1000));
             }
         }
 
-        /// <summary>
-        /// Setup the TrayIcon to close the app (cause there's no other way to close it)
-        /// </summary>
         private void SetupTrayIcon()
         {
             ContextMenuStrip trayMenu = new ContextMenuStrip();
@@ -410,28 +351,19 @@ namespace rans0m
             };
         }
 
-        // ----------------------------- EVENT HANDLERS -----------------------------
-
         private void Overlay_Load(object sender, EventArgs e)
         {
-            // Set the overlay to cover the entire screen
-            this.Bounds = SystemInformation.VirtualScreen; // Idk if ScreenBounds or this is better
+            this.Bounds = SystemInformation.VirtualScreen;
             this.Location = new Point(0, 0);
 
-            // Register .gold files and set the app to it's ready state
             FileTypeRegister.RegisterIconForExtension(".gold", Properties.Resources.GoldIco, "GoldFile");
             ResetRansom();
             SetupTrayIcon();
             SetupTopMostTimer();
 
-            // Starts the RansomLoop (in a new thread to avoid blocking the UI thread)
             new Thread(RansomLoop) { IsBackground = true }.Start();
         }
 
-        /// <summary>
-        /// TopMost alone doesn't stick, other windows (especially elevated ones) can still cover the overlay,
-        /// so this keeps shoving it back to the front without stealing focus/keyboard input
-        /// </summary>
         private void SetupTopMostTimer()
         {
             topMostTimer = new System.Windows.Forms.Timer { Interval = 500 };
@@ -448,7 +380,7 @@ namespace rans0m
 
         private void Overlay_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (Global.underRansom) // If the user is under ransom, prevent closing the overlay
+            if (Global.underRansom)
             {
                 e.Cancel = true;
                 return;
